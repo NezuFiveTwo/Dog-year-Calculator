cfc00b2e4d8fc9b2fe68973b31219ca478d6b904
NezuFiveTwo <72274001+NezuFiveTwo@users.noreply.github.com>
2021-11-17 11:20:15 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+//my age variable 
+let myAge = 24;
+
+//early age variable 
+let earlyYears = 2; 
+earlyYears = earlyYears * 10.5; 
+
+//start of dog years calculation 
+let laterYears = myAge - 2; 
+laterYears *= 4; 
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//end of the dog years calculation 
+let myAgeInDogYears = earlyYears + laterYears;  
+
+
+//Beginning of Strings
+myName = 'Eden Stapleton'.toLowerCase(); 
+
+console.log(`My name is ${myName}. I am ${myAge} in human years. However, I am ${myAgeInDogYears} in dog years!`)
